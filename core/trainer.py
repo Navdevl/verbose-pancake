7f7230b0f4e222b98d69fd5a77281ff75413bfe9
Naveen K <naveenhonestraj.k@happyfox.com>
2019-10-28 23:24:52 +0530

    updated
    Signed-off-by: Naveen K <naveenhonestraj.k@happyfox.com>

--- core/trainer.py
+++ core/trainer.py
@@ -5,8 +5,8 @@ import os
 import glob
 from PIL import Image
 from core.captcha_sequence import CaptchaSequence
-from tensorflow.keras.models import Model, Input
-from tensorflow.keras.layers import Conv2D, Dense, BatchNormalization, Activation, MaxPooling2D, Flatten
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Conv2D, Dense, BatchNormalization, Activation, MaxPooling2D, Flatten, Input
 from tensorflow.keras.callbacks import EarlyStopping, CSVLogger, ModelCheckpoint
 from tensorflow.keras.optimizers import Adam
 
@@ -16,28 +16,32 @@ class Trainer(object):
         self.characters = characters
         self.n_class = len(characters)
         self.n_len = 6
-        self.input_tensor = Input((height, width, 3))
-        self.train_data = CaptchaSequence(characters, batch_size=64, steps=1000)
-        self.valid_data = self.load_from_files(dir='test_data')
-        self.width = width
         self.height = height
+        self.width = width
+        self.input_tensor = Input((height, width, 3))
+        self.train_data = CaptchaSequence(characters, batch_size=64, steps=10)
+        self.valid_data = self.load_from_files(dir='test_data')
+        self.epochs = 1
+        self.create_model()
 
     def decode(self, y):
         y = np.argmax(np.array(y), axis=2)[:, 0]
         return ''.join([self.characters[x] for x in y])
 
     def load_from_files(self, dir):
+        print("Load from files..")
         test_data = []
-        filelist = glob.glob(os.path.join('dir', "*.png"))
+        filelist = glob.glob(os.path.join(dir, "*.png"))
+        print(filelist)
         for file in filelist:
-            test_data.append(self.load_from_file((file)))
+            test_data.append(self.load_from_file(file))
 
     def load_from_file(self, filename):
-        img_path = os.path.join("imgs", filename)
-        im = Image.open(img_path).convert('RGB')
+        im = Image.open(filename).convert('RGB')
         im = im.resize((self.width, self.height))
-        im_np = np.array(im) / 1.0
+        im_np = np.array(im) / 255.0
         im_np = im_np.reshape((1, self.height, self.width, 3))
+        print(filename.split('/')[-1].split('.')[0])
         return (im_np, filename.split('.')[0])
 
     def create_model(self):
@@ -53,23 +57,24 @@ class Trainer(object):
         x = [Dense(self.n_class, activation='softmax', name='c%d' % (i + 1))(x) for i in range(self.n_len)]
         self.model = Model(inputs=self.input_tensor, outputs=x)
 
-    def train(self, optimizer_value=1e-3):
-        callbacks = [EarlyStopping(patience=3), CSVLogger('cnn.csv'), ModelCheckpoint('cnn_best.h5', save_best_only=True)]
+    def train(self, optimizer_value=1e-4):
+        callbacks = [
+            EarlyStopping(patience=3), CSVLogger('cnn.csv'), ModelCheckpoint('cnn_best.h5', save_best_only=True)]
         self.model.compile(
             loss='categorical_crossentropy',
             optimizer=Adam(optimizer_value, amsgrad=True),
             metrics=['accuracy'])
         self.model.fit_generator(
             self.train_data,
-            epochs=10,
+            epochs=self.epochs,
             validation_data=self.valid_data,
             workers=8,
             use_multiprocessing=True,
             callbacks=callbacks)
 
     def execute(self):
-        self.train(optimizer_value=1e-3)
         self.train(optimizer_value=1e-4)
+        self.train(optimizer_value=1e-5)
         self.save()
 
     def save(self):
--- main.py
+++ main.py
@@ -1,16 +1,25 @@
-import glob
-import os
+# import glob
+# import os
+# from core.captcha_sequence import CaptchaSequence
 import string
-from core.captcha_sequence import CaptchaSequence
+from core.trainer import Trainer
+
+
+# if __name__ == '__main__':
+#     filelist = glob.glob(os.path.join('training_data', "*.png"))
+#     for f in filelist:
+#         os.remove(f)
+#     print("Cleaning successfully completed.")
+
+#     characters = string.digits
+#     cs = CaptchaSequence(characters, batch_size=10, steps=1, save_image=True)
+#     for X, y in cs:
+#         pass
 
 
 if __name__ == '__main__':
-    filelist = glob.glob(os.path.join('training_data', "*.png"))
-    for f in filelist:
-        os.remove(f)
-    print("Cleaning successfully completed.")
-
     characters = string.digits
-    cs = CaptchaSequence(characters, batch_size=10, steps=1, save_image=True)
-    for X, y in cs:
-        pass
+    height = 35
+    width = 135
+    trainer = Trainer(characters=characters, height=height, width=width)
+    trainer.execute()
